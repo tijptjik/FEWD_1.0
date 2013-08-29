6a8c6f305c8749495082bf1556ce32d81dbaca3a
Diana Jou <dianatjou@gmail.com>
2013-08-29 19:10:18 +0800

    before class update

--- lesson13/labs/atmConditional/js/atm.js
+++ lesson13/labs/atmConditional/js/atm.js
@@ -9,3 +9,76 @@ This is a banking application. This program will update your balance with your d
 -	Depending on their transaction choice either add or substract the balance. 
 -	Print the new balance to the console.
 */
+var balance = 1000;
+var transaction;
+var amount;
+
+transaction = prompt('(d)eposit, (w)ithdraw? or (q)uit');
+amount = parseInt(prompt('How much?'));
+
+do {
+
+	transaction = prompt('(d)eposit, (w)ithdraw? or (q)uit');
+
+	if (transaction == 'd') {
+		amount = parseInt(prompt('How much'))
+		balance = balance + amount;
+
+	} else if (transaction == 'w'){
+		amount = parseInt(prompt('How much'))
+		balance = balance - amount;
+
+	} else if {transaction =='q'{
+		console.log('Thanks for using our bank service!');
+		console.log('Your new balance: $' + balance);
+
+	} else {
+		console.log('error on input!');
+	}
+
+
+} while (transaction != 'q')
+
+
+
+while (transaction != 'q'){
+
+	transaction = prompt('(d)eposit, (w)ithdraw? or (q)uit');
+
+	if (transaction == 'd') {
+		amount = parseInt(prompt('How much'))
+		balance = balance + amount;
+
+	} else if (transaction == 'w'){
+		amount = parseInt(prompt('How much'))
+		balance = balance - amount;
+
+	} else if {transaction =='q'{
+		console.log('Thanks for using our bank service!');
+		console.log('Your new balance: $' + balance);
+
+	} else {
+		console.log('error on input!');
+	}
+}
+
+for (null; transaction != 'd'; null){
+	transaction = prompt('(d)eposit, (w)ithdraw? or (q)uit');
+
+	if (transaction == 'd') {
+		amount = parseInt(prompt('How much'))
+		balance = balance + amount;
+
+	} else if (transaction == 'w'){
+		amount = parseInt(prompt('How much'))
+		balance = balance - amount;
+
+	} else if {transaction =='q'{
+		console.log('Thanks for using our bank service!');
+		console.log('Your new balance: $' + balance);
+
+	} else {
+		console.log('error on input!');
+}
+}
+
