04551f44779a6a6a8f2e18d84179ff96c01a67a5
Mart van de Ven <m@type.hk>
2013-09-02 21:03:21 +0800

    Implemented module pattern for Station, corrected some functions

--- final/Diana/Dingding/js/app.js
+++ final/Diana/Dingding/js/app.js
@@ -1,30 +1,3 @@
-var DingDing = function(stations){
-	this.stations = stations.map(function(s){ new Station(s)})
-}
-
-var Station = function(obj){
-	for (var prop in obj) {
-     if (obj.hasOwnProperty(prop)) {
-       this[prop] = obj[prop];
-     }
-   }
-
-	this.print = function(){
-		console.log(this.name, 'at' , JSON.stringify(this.coordinates));
-	}
-
-	this.visitNext = function(){
-		DingDing.stations.map(function(s){
-			s.sequence - this.sequence - 
-		})
-	}
-	this.visitPrev= function(){
-		DingDing.stations.map(function(s){
-			s.sequence - this.sequence - 
-		})
-	}
-}
-
 var stations = [
 	{
 		name:'Sheung Wan',
@@ -32,16 +5,55 @@ var stations = [
 		sequence:67,
 		media:'sw.mp4',
 		caption:'Fruits of the seas',
-	}
-
-	name:'Sheung Wan',
+	},{
+		name:'Sheung Wan',
 		coordinates: [48.40129,8.3823],
 		sequence:67,
 		media:'sw.mp4',
 		caption:'Fruits of the seas',
 	}
-
-
 ]
+
+var DingDing = (function(obj){
+	stations = obj.map(function(s){ return new Station(s)})
+	// We use the stations || {} to pass in the stations variable that you've defined,
+	// or (||) if it can't find it it will pass an empty object and your script won't 
+	// because of it! :)
+	return {
+		stations : stations
+	}
+}(stations || {}))
+
+function Station(obj){
+	for (var prop in obj) {
+		if (obj.hasOwnProperty(prop)) {
+			this[prop] = obj[prop];
+		}
+	}
+   	// If we user CLASS.prototype.METHOD it is only stored
+   	// once in memory but we can use it for all objects that
+   	// are created
+   	Station.prototype.toString = function() {
+   		return this.name + ' @ [' + this.coordinates.toString() + ']'
+   	};
+	
+	Station.prototype.print = function(msg){
+		console.log(msg);
+	}
+
+	Station.prototype.visitNext = function(){
+		DingDing.stations.map(function(s){
+			s.sequence - this.sequence
+		})
+	}
+
+	Station.prototype.visitPrev = function(){
+		DingDing.stations.map(function(s){
+			this.sequence - s.sequence
+		})
+	}
+}
+
+
 var SheungWan = new Station(stations[0])
 
