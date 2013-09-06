a49972eaed9a2f626b2bf83c0af06f426f6d148e
Yuen Ying Kit <yingkityuen@gmail.com>
2013-09-06 16:02:46 +0800

    Prepare lesson 17 and 18.

---
+++ lesson17/labs/keyboardEvents/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Welcome to Keyboard Events Exercise
+ * A couple of thing we want to do:
+ * 
+ * 1) First, let's display the most recent keyCode the user pressed. This will help you do the second
+ *    exercise. 
+ *
+ * 2) Now, let's change the behavior of the "noVowels" textbox so that no vowels can
+ *    be typed in it. 
+ *    Hint, remeber what event.preventDefault() does? 
+ */
---
+++ lesson17/labs/mouseEvents/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Welcome to Mouse Events Exercise
+ * A couple of thing we want to do:
+ * 
+ * 1) When a user clicks the mousebox, turn it red by adding the "clicked" css class 
+ *    to it. When the user clicks it again, remove the "clicked" class and turn it blue 
+ *    again.
+ *
+ * 2) Use the mouseMove event to show the current position of the mouse using the HTML snippet provided
+ */
+
