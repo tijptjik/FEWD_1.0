eb66c2d5a1f6a77b92fc6fb9801474fe40fbf830
Mart van de Ven <m@type.hk>
2013-09-13 20:23:50 +0400

    Use the .eq() method to select nth elements, zero-indexed

--- final/HeidiLaw/Sandbox/js/scriptsquare.js
+++ final/HeidiLaw/Sandbox/js/scriptsquare.js
@@ -1,27 +1,28 @@
-console.log('ok');
+$(function(){ // Document-ready shorthand
+	
+	$('.boxes:eq(4)').css({ //select boxes by index specified as 4: WORKS
+    	backgroundColor: "black"
+	})
 
-//select boxes by index specified as 4: WORKS
-$('.boxes:nth-of-type(4)').css({
-    backgroundColor: "black"
-})
+	//attempt to dynamically pass index number to select boxes; DOES NOT WORK
+	// 1. nth-of-type does not take dynamic arguments?
 
-//attempt to dynamically pass index number to select boxes; DOES NOT WORK
-// 1. nth-of-type does not take dynamic arguments?
+	$('.boxes:nth-of-type("+colourCounter+")').css({
+	    backgroundColor: "+colourPalette[colourCounter]+"
+	})
 
-$('.boxes:nth-of-type("+colourCounter+")').css({
-    backgroundColor: "+colourPalette[colourCounter]+"
-})
+	var colourPalette = [
+	    'rgb(255,154,0)',
+	    'rgb(255,198,0)',
+	    'rgb(255,89,0)'
+	]
 
-var colourPalette = [
-    'rgb(255,154,0)',
-    'rgb(255,198,0)',
-    'rgb(255,89,0)'
-]
+	colourCounter = 0;
 
-colourCounter = 0;
+	$('body').on('click', function () {
+	    index = colourCounter % colourPalette.length
+	    $(this).css('background-color', colourPalette[index]);
+	    colourCounter++;
+	})
 
-$('body').on('click', function () {
-    index = colourCounter % colourPalette.length
-    $(this).css('background-color', colourPalette[index]);
-    colourCounter++;
 })
