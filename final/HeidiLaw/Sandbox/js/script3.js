982cf8099aae35f0762613cf9def463bcd91113e
Heidi Law <heidilaw4@gmail.com>
2013-09-13 12:09:47 +0800

    - fixed overflow problem of beehive

--- final/HeidiLaw/Sandbox/js/script3.js
+++ final/HeidiLaw/Sandbox/js/script3.js
@@ -1,37 +1,51 @@
-console.log("check");
-
 var colourPalette = [
-	'rgb(255,153,0)',
-	'rgb(191,134,48)',
-	'rgb(166,99,0)',
-	'rgb(255,178,64)',
-	'rgb(255,199,115)'
-	]
+    'rgb(255,154,0)',
+    'rgb(255,198,0)',
+    'rgb(255,89,0)'
+]
 
+$('body').css({
+    backgroundColor: "-webkit-linear-gradient(bottom, rgb(139,214,186) 22%, rgb(167,255,224) 61%, rgb(201,255,255) 81%)"
+})
 
+//RESPONSIVE TO THE 'CLICK' EVENT
 
 colourCounter = 0;
 
-$('body').css({backgroundColor: colourPalette[1]})
 
-$('body').css({
-    background: "-webkit-gradient(linear, left top, left bottom, from("+colourPalette[3]+"), to("+colourPalette[4]+"))" 
-});
 
-// $('body').css({
-//     background: "-webkit-gradient(linear, left top, left bottom, from(rgb(255,178,64)), to(rgb(255,199,115))" 
-// });
-
-//looping through the colourPalette indexes
-$('body').on('click', function(){
-	index = colourCounter % colourPalette.length
-	$(this).css('background-color', colourPalette[index]);
-	colourCounter++;
+$('body').on('click', function () {
+    index = colourCounter % colourPalette.length
+    $(this).css('background-color', colourPalette[index]);
+    colourCounter++;
 })
 
-//make values responsive to mousemove
-$('body').on('mousemove', valueIncrease)
 
-	function valueIncrease(val){ 
-		colourCounter = colourCounter ++
-	}
+$('body').on('click', function () {
+    index = colourCounter % colourPalette.length
+    $(this).css('background-color', -webkit-linear-gradient(bottom, "+'colourPalette[index]'+" 22%, "colourPalette[index + 1]" 61%, rgb(201,255,255) 81%));
+    colourCounter++;
+})
+
+//salvaged
+// +$('body').css({backgroundColor: colourPalette[1]})
+// +
+// +$('body').css({
+// +    background: "-webkit-gradient(linear, left top, left bottom, from("+colourPalette[3]+"), to("+colourPalette[4]+"))" 
+// +});
+// +
+
+//  $('body').on('click', function(){
+//    index = colourCounter % colourPalette.length
+//    $(this).css('background-color', colourPalette[index]);
+//    colourCounter++;
+// -
+//  })
+ 
+// -console.log("read")
+// +//make values responsive to mousemove
+// +$('body').on('mousemove', valueIncrease)
+// +
+// +  function valueIncrease(val){ 
+// +    colourCounter = colourCounter ++
+// +  }
