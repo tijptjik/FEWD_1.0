c70e947de2760702d8531e047a0cc821ca751bc6
Mart van de Ven <m@type.hk>
2013-09-13 20:41:04 +0400

    Some Cycling logic examples with comments

--- final/HeidiLaw/Sandbox/js/scriptsquare.js
+++ final/HeidiLaw/Sandbox/js/scriptsquare.js
@@ -1,28 +1,38 @@
-var colourPalette = [
-    'rgb(255,154,0)',
-    'rgb(255,198,0)',
-    'rgb(255,89,0)'
-]
+var colourCounter = 0;
+var colourPalette = [ // be careful to add spaces if you want to match colours
+    'rgb(255, 154, 0)',
+    'rgb(255, 198, 0)',
+    'rgb(255, 89, 0)']
 
 $(function(){ // Document-ready shorthand
 
-	$('.boxes:eq(4)').css({ //select boxes by index specified as 4: WORKS
-    	backgroundColor: "black"
+	$('.boxes').each(function(i, elem){ 
+	// for each box loop through the colourPallette, and  provide set the 
+	// background to the next colour. So cool! Try resizing, nice effect :)
+		idx = i % colourPalette.length
+		$(elem).css('background-color', colourPalette[idx])
 	})
-
-	//attempt to dynamically pass index number to select boxes; DOES NOT WORK
-	// 1. nth-of-type does not take dynamic arguments?
-
-	$('.boxes:nth-of-type("+colourCounter+")').css({
-	    backgroundColor: "+colourPalette[colourCounter]+"
-	})
-
-	colourCounter = 0;
 
 	$('body').on('click', function () {
-	    index = colourCounter % colourPalette.length
-	    $(this).css('background-color', colourPalette[index]);
+		// Set the background, looping through the colour using a colour.
+		// Counter isn't needed if you use indexOf(colour) like I did in 
+		// the next example.
+	    idx = colourCounter % colourPalette.length
+	    $(this).css('background-color', colourPalette[idx]);
 	    colourCounter++;
+	})
+
+	$('body').on('click','.boxes', function(e){
+		// cycle through the colours when clicking on individual boxes
+		// oh yeah! Checks colour first, then checks what's the next 
+		// colour in the array, then sets the colour to the next one.
+		colour = $(this).css('background-color')
+		next = (colourPalette.indexOf(colour) + 1) % colourPalette.length 
+		$(this).css('background-color', colourPalette[next])
+	})
+
+	$('.boxes:eq(4)').css({ // set the 5th box as black - how dark! 
+    	backgroundColor: "black"
 	})
 
 })
